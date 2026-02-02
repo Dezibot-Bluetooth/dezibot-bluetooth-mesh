0557ef9bff7b4720cbfdcfe9157fff389eedfa17
gregorscholz <gregorscholz@proton.me>
2026-02-02 23:01:36 +0100

    fix: resolve rebase errors

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "lib/client.h"
+#include "lib/bluetooth.h"
 
 #define TAG "MAIN"
 
@@ -13,12 +16,18 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(err);
 
-    //ESP_LOGI(TAG, "Starting BLE Mesh Client...");
+    ESP_LOGI(TAG, "Starting BLE Mesh Client...");
 
-    //ESP_ERROR_CHECK(ble_mesh_client_init());
+    ESP_ERROR_CHECK(bluetooth_init());
+    ESP_ERROR_CHECK(ble_mesh_client_init());
+
+    // Give provisioning time to complete
+    vTaskDelay(pdMS_TO_TICKS(5000));
 
     while (1) {
-        ble_mesh_client_send(1);
-        ble_mesh_client_send(0);
+        ble_mesh_client_send(1, 0x0002);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        ble_mesh_client_send(0, 0x0002);
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
