63ad150d5fb447fa3d7a98764753ceae6bc849b4
gregorscholz <gregorscholz@proton.me>
2026-02-25 22:58:16 +0100

    add: new client methods to wrapper class

--- components/dezibot_bluetooth_mesh/DeziBotMesh.cpp
+++ components/dezibot_bluetooth_mesh/DeziBotMesh.cpp
@@ -92,14 +92,255 @@ bool DeziBotMesh::beginServer(mesh_server_evt_cb_t serverCallback) {
     return true;
 }
 
-void DeziBotMesh::sendOnOff(bool onoff, uint16_t addr) {
-    if (_mode == MODE_NODE) {
-        // TODO
-    } else if (_mode == MODE_CLIENT_ONLY) {
-        mesh_bridge_client_send(onoff ? 1 : 0, addr);
-    } else {
-        ESP_LOGW("DeziBotMesh", "Cannot send in server-only mode");
+esp_err_t DeziBotMesh::getOnOff(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get onoff in server-only mode");
+        return ESP_ERR_INVALID_STATE;
     }
+    return mesh_bridge_client_get_onoff(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setOnOff(bool onoff, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set onoff in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_onoff(onoff ? 1 : 0, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getLevel(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get level in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_level(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setLevel(int16_t level, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set level in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_level(level, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setDelta(int32_t delta, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set delta in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_delta(delta, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setMove(int16_t move, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set move in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_move(move, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getDefaultTransitionTime(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get default transition time in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_default_transition_time(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setDefaultTransitionTime(uint8_t transitionTime, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set default transition time in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_default_transition_time(transitionTime, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getOnPowerUp(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get onpowerup in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_onpowerup(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setOnPowerUp(uint8_t power, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set onpowerup in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_onpowerup(power, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getPowerLevel(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get power level in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_power_level(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setPowerLevel(uint16_t power, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set power level in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_power_level(power, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getPowerDefault(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get power default in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_power_default(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setPowerDefault(uint16_t powerDefault, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set power default in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_power_default(powerDefault, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getPowerRange(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get power range in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_power_range(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setPowerRange(uint16_t min, uint16_t max, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set power range in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_power_range(min, max, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getBattery(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get battery in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_battery(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getLocGlobal(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get global location in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_loc_global(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setLocGlobal(int32_t latitude, int32_t longitude, int16_t altitude, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set global location in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_loc_global(latitude, longitude, altitude, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getLocLocal(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get local location in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_loc_local(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setLocLocal(int16_t north, int16_t east, int16_t altitude, uint8_t floorNumber,
+                                  uint16_t uncertainty, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set local location in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_loc_local(north, east, altitude, floorNumber, uncertainty, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getUserProperties(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get user properties in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_user_properties(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getUserProperty(uint16_t propertyId, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get user property in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_user_property(propertyId, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setUserProperty(uint16_t propertyId, uint8_t *propertyValue, uint16_t propertyValueLen,
+                                      uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set user property in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_user_property(propertyId, propertyValue, propertyValueLen, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getAdminProperties(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get admin properties in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_admin_properties(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getAdminProperty(uint16_t propertyId, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get admin property in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_admin_property(propertyId, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setAdminProperty(uint16_t propertyId, uint8_t adminAccess, uint8_t *propertyValue,
+                                       uint16_t propertyValueLen, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set admin property in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_admin_property(propertyId, adminAccess, propertyValue, propertyValueLen, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getManuProperties(uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get manufacturer properties in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_manu_properties(addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getManuProperty(uint16_t propertyId, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get manufacturer property in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_manu_property(propertyId, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::setManuProperty(uint16_t propertyId, uint8_t adminAccess, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot set manufacturer property in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_set_manu_property(propertyId, adminAccess, addr, elemIndex);
+}
+
+esp_err_t DeziBotMesh::getClientProperties(uint16_t propertyId, uint16_t addr, uint8_t elemIndex) {
+    if (_mode == MODE_SERVER_ONLY) {
+        ESP_LOGW("DeziBotMesh", "Cannot get client properties in server-only mode");
+        return ESP_ERR_INVALID_STATE;
+    }
+    return mesh_bridge_client_get_client_properties(propertyId, addr, elemIndex);
 }
 
 void DeziBotMesh::getDeviceUUID(uint8_t* uuid) {
--- components/dezibot_bluetooth_mesh/DeziBotMesh.h
+++ components/dezibot_bluetooth_mesh/DeziBotMesh.h
@@ -10,6 +10,7 @@
 #define DEZIBOT_MESH_ARDUINO_H
 
 #include <stdint.h>
+#include <esp_err.h>
 #include <server/server_events.h>
 
 /**
@@ -68,12 +69,374 @@ public:
     bool beginServer(mesh_server_evt_cb_t serverCallback);
 
     /**
-     * @brief Send Generic OnOff command to another node
-     * 
+     * @brief Send Generic OnOff Set Unacknowledged to another node
+     *
+     * Convenience wrapper for the most common client command.
+     *
      * @param onoff true=ON, false=OFF
      * @param addr Target unicast or group address
      */
     void sendOnOff(bool onoff, uint16_t addr);
+
+    /**
+     * @brief Get Generic OnOff state from another node
+     *
+     * Sends a Generic OnOff Get to the target address.
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getOnOff(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic OnOff state on another node
+     *
+     * Sends a Generic OnOff Set Unacknowledged to the target address.
+     *
+     * @param onoff true=ON, false=OFF
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setOnOff(bool onoff, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic Level state from another node
+     *
+     * Sends a Generic Level Get to the target address.
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getLevel(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic Level state on another node
+     *
+     * Sends a Generic Level Set Unacknowledged to the target address.
+     *
+     * @param level Target level value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setLevel(int16_t level, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Send Generic Delta Set Unacknowledged to another node
+     *
+     * @param delta Delta level value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setDelta(int32_t delta, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Send Generic Move Set Unacknowledged to another node
+     *
+     * @param move Move delta level value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setMove(int16_t move, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic Default Transition Time from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getDefaultTransitionTime(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic Default Transition Time on another node
+     *
+     * @param transitionTime Transition time value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setDefaultTransitionTime(uint8_t transitionTime, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic OnPowerUp state from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getOnPowerUp(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic OnPowerUp state on another node
+     *
+     * @param power OnPowerUp value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setOnPowerUp(uint8_t power, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic Power Level state from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getPowerLevel(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic Power Level on another node
+     *
+     * @param power Target power value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setPowerLevel(uint16_t power, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic Power Default from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getPowerDefault(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic Power Default on another node
+     *
+     * @param powerDefault Target power default value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setPowerDefault(uint16_t powerDefault, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic Power Range from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getPowerRange(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic Power Range on another node
+     *
+     * @param min Minimum power value
+     * @param max Maximum power value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setPowerRange(uint16_t min, uint16_t max, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic Battery state from another node
+     *
+     * Sends a Generic Battery Get to the target address.
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getBattery(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic Global Location state from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getLocGlobal(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic Global Location state on another node
+     *
+     * @param latitude Global latitude value
+     * @param longitude Global longitude value
+     * @param altitude Global altitude value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setLocGlobal(int32_t latitude, int32_t longitude, int16_t altitude, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get Generic Local Location state from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getLocLocal(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set Generic Local Location state on another node
+     *
+     * @param north Local north value
+     * @param east Local east value
+     * @param altitude Local altitude value
+     * @param floorNumber Local floor number value
+     * @param uncertainty Uncertainty value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setLocLocal(int16_t north, int16_t east, int16_t altitude, uint8_t floorNumber,
+                          uint16_t uncertainty, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get list of Generic User Property IDs from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getUserProperties(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get a Generic User Property value from another node
+     *
+     * @param propertyId Property ID
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getUserProperty(uint16_t propertyId, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set a Generic User Property value on another node
+     *
+     * Sends a Generic User Property Set Unacknowledged.
+     *
+     * @param propertyId Property ID
+     * @param propertyValue Pointer to the property value buffer
+     * @param propertyValueLen Length of the property value buffer
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setUserProperty(uint16_t propertyId, uint8_t *propertyValue, uint16_t propertyValueLen,
+                              uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get list of Generic Admin Property IDs from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getAdminProperties(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get a Generic Admin Property value from another node
+     *
+     * @param propertyId Property ID
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getAdminProperty(uint16_t propertyId, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set a Generic Admin Property value on another node
+     *
+     * Sends a Generic Admin Property Set Unacknowledged.
+     *
+     * @param propertyId Property ID
+     * @param adminAccess Admin access value
+     * @param propertyValue Pointer to the property value buffer
+     * @param propertyValueLen Length of the property value buffer
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setAdminProperty(uint16_t propertyId, uint8_t adminAccess, uint8_t *propertyValue,
+                               uint16_t propertyValueLen, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get list of Generic Manufacturer Property IDs from another node
+     *
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getManuProperties(uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get a Generic Manufacturer Property value from another node
+     *
+     * @param propertyId Property ID
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getManuProperty(uint16_t propertyId, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Set a Generic Manufacturer Property value on another node
+     *
+     * Sends a Generic Manufacturer Property Set Unacknowledged.
+     *
+     * @param propertyId Property ID
+     * @param adminAccess Admin access value
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t setManuProperty(uint16_t propertyId, uint8_t adminAccess, uint16_t addr, uint8_t elemIndex = 0);
+
+    /**
+     * @brief Get list of Generic Client Property IDs from another node
+     *
+     * @param propertyId Client property ID
+     * @param addr Target unicast or group address
+     * @param elemIndex Local element index to use (default: 0)
+     *
+     * @return ESP_OK on success, error code otherwise
+     */
+    esp_err_t getClientProperties(uint16_t propertyId, uint16_t addr, uint8_t elemIndex = 0);
 
     /**
      * @brief Get device UUID
